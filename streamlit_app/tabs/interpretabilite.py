cb6c2884b0e4023ca3792fec8a914ae134ca360e
Christelle Ramarokoto <me@christelle-ramarokoto.com>
2025-09-30 15:30:17 +0200

    Updates resnet interpretability

--- streamlit_app/tabs/interpretabilite.py
+++ streamlit_app/tabs/interpretabilite.py
@@ -9,5 +9,48 @@ def run():
     st.image("assets/analysis.svg")
 
     st.title(title)
-    st.markdown("---")
-    st.write("Expliquez ici les méthodes d'interprétabilité (SHAP, LIME, etc.).")
+    
+    tab1, tab2 = st.tabs(["Fine tuned Resnet", "Logistic Regression"])
+
+    with tab1:
+        st.markdown("#### Fine tuned Resnet")
+        st.markdown(
+            """
+            Afin de vérifier la capacité du modèle à classifier les images en utilisant des données utiles, nous étudions l'interprétabilité du modèle via un grad cam.
+
+            Nous observons les limitations du modèle :
+            - certains objets sont reconnus en tant que tel mais leur catégorie est mauvaise car ils peuvent appartenir à plusieurs catégories naturellement
+            - certains objets sont mal représentés dans les images (par exemple lot de cartouches de jeu trop dense)
+            - certaines catégories se basent plus sur l'environnement que sur l'objet (par exemple pour les articles de jardinage), ce qui n'est pas stable et forcément discriminant
+            """
+        )
+        st.image("assets/grad_cam/gc1.png")
+        st.image("assets/grad_cam/gc2.png")
+        st.image("assets/grad_cam/gc3.png")
+        st.image("assets/grad_cam/gc4.png")
+        st.image("assets/grad_cam/gc5.png")
+        st.image("assets/grad_cam/gc6.png")
+        st.image("assets/grad_cam/gc7.png")
+        st.image("assets/grad_cam/gc8.png")
+        st.image("assets/grad_cam/gc9.png")
+        st.image("assets/grad_cam/gc10.png")
+        st.image("assets/grad_cam/gc11.png")
+        st.image("assets/grad_cam/gc12.png")
+        st.image("assets/grad_cam/gc13.png")
+        st.image("assets/grad_cam/gc14.png")
+        st.image("assets/grad_cam/gc15.png")
+        st.image("assets/grad_cam/gc16.png")
+        st.image("assets/grad_cam/gc17.png")
+        st.image("assets/grad_cam/gc18.png")
+        st.image("assets/grad_cam/gc19.png")
+        st.image("assets/grad_cam/gc20.png")
+        st.image("assets/grad_cam/gc21.png")
+        st.image("assets/grad_cam/gc22.png")
+        st.image("assets/grad_cam/gc23.png")
+        st.image("assets/grad_cam/gc24.png")
+        st.image("assets/grad_cam/gc25.png")
+        st.image("assets/grad_cam/gc26.png")
+        st.image("assets/grad_cam/gc27.png")
+
+    with tab2:
+        st.markdown("#### Logistic Regression")
